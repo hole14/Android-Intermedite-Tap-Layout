e007c50040310b4f2570aa26ad4a5e1f76629e1c
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-06 10:04:54 +0700

    Menambahkan onViewCreated pada HomeFragment.kt

--- app/src/main/java/com/example/mytablayout/HomeFragment.kt
+++ app/src/main/java/com/example/mytablayout/HomeFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class HomeFragment : Fragment() {
+
+    companion object{
+        const val ARG_SECTION_NUMBER = "section_number"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,4 +21,10 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val tvLabel: TextView = view.findViewById(R.id.section_label)
+        val index = arguments?.getInt(ARG_SECTION_NUMBER, 0)
+        tvLabel.text = getString(R.string.content_tab_text, index)
+    }
 }
